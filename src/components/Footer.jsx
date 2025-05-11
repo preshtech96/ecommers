824e269c543fe12a877e7a5d57263f429405a2da
preshtech96 <preshtech96@gmail.com>
2025-05-11 23:26:55 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,15 @@ import preshtech from "../assets/preshtech.png";
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center gap-2 bg-purple-950'>
-        <div><img className='h-12 w-12 object-contain' src={preshtech} alt="" /></div>
-        <div className='text-white'>Copyright © 2025 Preshtech. All rights reserved.</div>
+    <div className='flex flex-col sm:flex-row justify-center items-center gap-2 bg-purple-950 p-4 text-center sm:text-left'>
+      <div>
+        <img className='h-12 w-12 object-contain' src={preshtech} alt="Preshtech Logo" />
+      </div>
+      <div className='text-white text-sm sm:text-base'>
+        © 2025 Preshtech. All rights reserved.
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
